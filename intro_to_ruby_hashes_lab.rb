5747e4a951f67c707699ffda51f0cdbd8b5373dc
haider <haider.shah@hotmail.com>
2019-09-10 13:59:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,7 @@ def monopoly_with_second_tier
   # complexity.
   e={}
   	monopoly =  {:railroads => e}
-  	monopoly[:railroads]=:pieces
+  	monopoly[:railroads][e]=:pieces
 end
 
 monopoly_with_second_tier
